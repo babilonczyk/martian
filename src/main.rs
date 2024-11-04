7a85070a6bc359288c2dcfba8ba4b7c1d84d935e
Jan Piotrzkowski <piotrzkowski.jan@gmail.com>
2024-11-04 17:39:34 +0100

    Clean main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,1 @@
-use martian::time::*;
-
-fn main() {
-    let sol = current_sol(false).unwrap();
-
-    println!("Current Mars Sol Date (MSD): {}", sol);
-
-    let mtc = mtc_now(false).unwrap();
-
-    println!("Current Martian Coordinated Time (MTC): {}", mtc);
-}
+fn main() {}
